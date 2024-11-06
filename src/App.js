78a3bc7b777d2a08124fd30cc801bffd355347ab
Eren AVAR <eren.avar@hotmail.com>
2024-11-06 18:51:23 +0300

    create home screen and display carousel in it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import HomeScreen from './components/HomeScreen';
 
 function App() {
   return (
     <div className="App">
     
-    Hello World
+    <HomeScreen/>
 
     </div>
   );
