b991a1f208c31ce47dad23a5c13db57af8d05836
Eren AVAR <eren.avar@hotmail.com>
2024-11-07 22:21:54 +0300

    add slider arrows

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs'
 import "./Carousel.css";
+
+
 
 
 function Carousel({ data }) {
 
   return (
     <div className='carousel'>
+      <BsArrowLeftCircleFill />
       {data.map((item, index) => {
         return <img src={item.src} alt={item.alt} key={index} className="slide" />
-
-      })}</div>
+      })}
+      <BsArrowRightCircleFill />
+    </div>
   )
 }
 
