6a92c3a8dce81a7e921139afa647d2da19faf1b7
Eren AVAR <eren.avar@hotmail.com>
2024-11-08 21:37:17 +0300

    add inactive indicator and provide one photo

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs'
 import "./Carousel.css";
 
 function Carousel({ data }) {
-
+  const [slide, setSlide] = useState(0)
   return (
     <div className='carousel'>
       <BsArrowLeftCircleFill className="arrow arrow-left" />
       {data.map((item, index) => {
-        return <img src={item.src} alt={item.alt} key={index} className="slide" />
+        return <img src={item.src} alt={item.alt} key={index} className={index ? "slide" : "slide slide-hidden"} />
       })}
       <BsArrowRightCircleFill className="arrow arrow-right" />
       <span className="indicators">
         {data.map((item, i) => {
-          return <button key={i} className="indicator"></button>
+          return <button key={i} className={slide === i ? 'indicator' ? 'indicator indicator-inactive'}></button>
         })}
       </span>
     </div >
