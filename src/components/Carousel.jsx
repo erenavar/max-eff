eb707e94205c8c7cdd4de32b6c7d60a6b6489ea9
Eren AVAR <eren.avar@hotmail.com>
2024-11-08 23:25:00 +0300

    complete carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,23 +1,49 @@
-import React, { useState } from 'react';
-import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs'
+import React, { useState, useEffect } from 'react';
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs';
 import "./Carousel.css";
 
 function Carousel({ data }) {
-  const [slide, setSlide] = useState(0)
+  const [slide, setSlide] = useState(0);
+
+  const nextSlide = () => {
+    setSlide((prevSlide) => (prevSlide + 1) % data.length);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [data.length]);
+
   return (
-    <div className='carousel'>
-      <BsArrowLeftCircleFill className="arrow arrow-left" />
-      {data.map((item, index) => {
-        return <img src={item.src} alt={item.alt} key={index} className={slide === index ? "slide" : "slide slide-hidden"} />
-      })}
-      <BsArrowRightCircleFill className="arrow arrow-right" />
+    <div className="carousel">
+      <BsArrowLeftCircleFill className="arrow arrow-left" onClick={() => setSlide((slide - 1 + data.length) % data.length)} />
+
+      {data.map((item, index) => (
+        <div key={index} className={slide === index ? "slide" : "slide slide-hidden"}>
+          <img src={item.src} alt={item.alt} className="slide-image" />
+          <div className="slide-content">
+            <h2 className="slide-title">{item.title}</h2>
+            <p className="slide-description">{item.description}</p>
+          </div>
+        </div>
+      ))}
+
+      <BsArrowRightCircleFill className="arrow arrow-right" onClick={nextSlide} />
+
       <span className="indicators">
-        {data.map((item, i) => {
-          return <button key={i} onClick={null} className={slide === i ? 'indicator' : 'indicator indicator-inactive'}></button>
-        })}
+        {data.map((_, i) => (
+          <button
+            key={i}
+            className={slide === i ? 'indicator' : 'indicator indicator-inactive'}
+            onClick={() => setSlide(i)}
+          />
+        ))}
       </span>
-    </div >
-  )
+    </div>
+  );
 }
 
-export default Carousel
+export default Carousel;
--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Carousel from './Carousel'
 import Slides from '../data/carouselData.json'
-
+import "./Carousel.css";
 
 export default function HomeScreen() {
   const { slides } = Slides;
   return (
     <Carousel data={slides} />
-  )
+  );
 }
+
+
+
