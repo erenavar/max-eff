4f86169e84070bc4696f64f986068856e56f6bcc
Eren AVAR <eren.avar@hotmail.com>
2024-11-17 23:22:29 +0300

    add font awesome cdn link and create navbar titles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <header className="header">
+        <a href="#" className="logo">
+          LOGO
+        </a>
+        <nav>
+          <ul className="menu-items">
+            <li>
+              <a href="#" className="menu-item">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Dropdown Menu
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Mega
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                Image Menu
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-item">
+                About
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
 export default Navbar;
