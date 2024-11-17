24803c292bec1f17a0228f41f1b5ed3bdde47a0b
Eren AVAR <eren.avar@hotmail.com>
2024-11-17 22:16:37 +0300

    create navbar component file and show on home screen

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import Carousel from './Carousel'
-import Slides from '../data/carouselData.json'
+import React from "react";
+import Carousel from "./Carousel";
+import Slides from "../data/carouselData.json";
 import "./Carousel.css";
+import Navbar from "./Navbar";
 
 export default function HomeScreen() {
   const { slides } = Slides;
   return (
-    <Carousel data={slides} />
+    <div>
+      <Navbar />
+      <Carousel data={slides} />
+    </div>
   );
 }
-
-
-
